be13f414a69fc0f94d743d098a9841d0fb06fda5
James Broomfield <james.broomfield@assembler.app>
2022-01-31 16:28:55 +1100

    Minor cleanup

--- src/svelte/fields/NewProperty.svelte
+++ src/svelte/fields/NewProperty.svelte
@@ -36,22 +36,20 @@
 </div>
 
 <div class="form-control">
-<label for="new-property-type" class="label">
-  <span class="label-text">Type</span>
-</label> 
-<!-- svelte-ignore a11y-no-onchange -->
-<select
-  class="select select-bordered w-full max-w-xs"
-  bind:value={schema.properties.type}
-  on:change={validateSchema}
->
-  <option disabled="disabled" value="">Type</option> 
-  <option value="string">String</option> 
-  <option value="number">Number</option>
-  <option value="boolean">Boolean</option>
-  <option value="array">Array</option>
-  <option value="object">Object</option>
-</select>
+  <label for="new-property-type" class="label">
+    <span class="label-text">Type</span>
+  </label> 
+  <!-- svelte-ignore a11y-no-onchange -->
+  <select
+    class="select select-bordered w-full max-w-xs"
+    bind:value={schema.properties.type}
+    on:change={validateSchema}
+  >
+    <option disabled="disabled" value="">Type</option> 
+    <option value="string">String</option> 
+    <option value="number">Number</option>
+    <option value="boolean">Boolean</option>
+    <option value="array">Array</option>
+    <option value="object">Object</option>
+  </select>
 </div>
-
-<div><pre>{JSON.stringify(schema, null, 4)}</pre></div>
